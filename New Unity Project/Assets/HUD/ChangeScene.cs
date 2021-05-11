dc0aa59d20a35827b851bf8b9e6ae3ba1a6b9252
Eugene Denisovets <denisovezz123@gmail.com>
2021-05-11 10:59:55 +0300

    second commit
    Main

---
+++ New Unity Project/Assets/HUD/ChangeScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameLevel");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            MainMenu();
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
